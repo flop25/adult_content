b1b3aa7470fada24bd10e046832a5cf2d0a9b5c3
riopwg <rio@piwigo.org>
2009-11-27 21:07:27 +0000

    language IT
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@4374 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -1,40 +1,40 @@
 <?php
-$lang['ac_title_menu'] = 'Attention';
+$lang['ac_title_menu'] = 'Attenzione';
 $lang['ac_title_menu_statut'] ='Information';
-$lang['ac_text'] = 'Cette galerie contient des images s\'addressant à des personnes matures et/ou majeurs';
-$lang['ac_lien'] = 'enlever la protection';
-$lang['ac_identif'] = 'ou s\'identifier';
-$lang['ac_fermer'] = 'ou ne plus afficher';
+$lang['ac_text'] = 'Questa galleria contiene delle immagini destinati ad un pubbilico adulto';
+$lang['ac_lien'] = 'togliere la protezzione';
+$lang['ac_identif'] = 'o identificarsi';
+$lang['ac_fermer'] = 'o non più visualizzare';
 
-$lang['ac_title_fermer'] = 'contenu filtré';
+$lang['ac_title_fermer'] = 'contenuto filtrato';
 
-$lang['ac_user_text_18'] = 'Vous avez plus de 18 ans';
-$lang['ac_user_text_16'] = 'Vous avez 16 ou 17 ans';
-$lang['ac_logout'] = 'ou sinon cliquez ici';
+$lang['ac_user_text_18'] = 'Avete più di 18 anni';
+$lang['ac_user_text_16'] = 'Avete 16 o 17 anni';
+$lang['ac_logout'] = 'o allora cliccare qui';
 
-$lang['ac_user_no'] = "vous n'avez pas souhaité activer ce genre de contenu";
-$lang['ac_user_no_s'] = "Filtre activé";
-$lang['ac_statut'] ='Votre statut :';
+$lang['ac_user_no'] = "non avete desiderato attivare questo tipo di contenuti";
+$lang['ac_user_no_s'] = "Filtro attivato";
+$lang['ac_statut'] ='Vostro statuto :';
 
-$lang['ac_button_activ_18'] = 'Activer le contenu pornographique';
-$lang['ac_button_activ_16'] = 'Activer le contenu érotique';
-$lang['ac_text_charte'] = "Vous entrez sur un site dont le contenu visuel à caractère érotique et pornographique est destiné exclusivement à un public averti et peut présenter des contenus à caractère &eacute;rotique. Ce site s'adresse aux personnes majeures et consentantes. Ce présent site décline toute responsabilité quant à l'utilisation qui sera faite des contenus et services de ce site par toute personne mineure ou majeure susceptible d'être choquée par celle-ci. Toute personne acceptant d'entrer au sein de ce site strictement réservé &agrave; un public averti décharge ce présent site de toutes les conséquences de sa consultation. </p>
-<p> Si je viole l'un de ces engagements ou en cas de fausses déclaration, je pourrai être en violation avec les lois de mon pays, et <B>assumerai l'entière responsabilité de mes actes</B>. Je déclare avoir été pleinement informé du contenu proposé et renonce à toutes poursuites judiciaires contre le fournisseur d'accès et les éditeurs de ce site. J'ai lu attentivement les paragraphes ci-dessus et signe électroniquement mon accord avec ce qui précède en cliquant sur le bouton correspondant à mon âge.";
-$lang['ac_text_activ_18'] = "  <u>... et je certifie sur l'honneur que </u>: <br>
-- <B>J'ai plus de 18 ans et suis majeur</B> selon la loi en vigueur dans mon pays. <br>
-- Je suis averti que le programme propose des images à caractère pornographique et érotique. <br>
-- Les lois de mon pays m'autorisent à accéder à ce site.";
-$lang['ac_text_activ_16'] = "<u> ... et je certifie sur l'honneur que </u>: <br>
-        - <B>J'ai  16 ou 17 ans.</B> <br>
-- Je suis averti que ce site propose des images à caractère érotique. <br>
-- Les lois de mon pays m'autorisent à accéder à ce site.";
-$lang['ac_retour'] = 'Je ne souhaite pas activer ce genre de contenu et je retourne sur la page précédente';
-$lang['ac_retour_b'] = 'Je ne souhaite pas activer ce genre de contenu';
-$lang['ac_retour_c'] = 'Je retourne sur la page précédente';
+$lang['ac_button_activ_18'] = 'Attivare il contenuto pornografico';
+$lang['ac_button_activ_16'] = 'Attivare il contenuto erotico';
+$lang['ac_text_charte'] = "State entrando in un sito nel quale i contenuti a carattere erotico e/o pornografico sono destinati ad un pubblico adulto. Questo sito è di fatto destinato ad un pubblico maggiorenne. Il presente sito non può essere tenuto responsabile del uso che verra fatto dei contenuti e servizzi del suddetto sito. Accettando d'entrare in questo sito destinato ad un pubblico adulto, assumete di fatto tutte le conseguezze legate alla sua consultazzione. </p>
+<p> Se vado al incontro di quanto dichiarato o in caso di falza dichiarazione, potrei essere in violazione con le leggi del mio paese, e <B>ne assumo l'intera reponsabilità</B>. Dichiaro essere stato correttamente informato dei contenuti proposti e rinunciare a tutti i procedimenti nei confronti del provaider e del/degli autore/i di questo sito. Ho letto attentamente i paragrafi qui sopra e firmo elettronicamente il mio accordo con ciò che precede cliccando sul pulsante corrispondente alla mia età.";
+$lang['ac_text_activ_18'] = "  <u>... e certifico sul\'onore che </u>: <br>
+- <B>ho più di 18 anni e che sono maggiorenne </B> secondo le leggi in vigore nel mio paese. <br>
+- sono al corrente che il sito propone delle immagini a carattere pornografico e/o erotico. <br>
+- Le leggi del mio paese m'autorizzono ad accedere a questo sito.";
+$lang['ac_text_activ_16'] = "<u> ... e certifico sul\'onore che </u>: <br>
+        - <B>ho 16 o 17 anni.</B> <br>
+- sono al corrente che il sito propone delle immagini a carattere erotico. <br>
+- Le leggi del mio paese m'autorizzono ad accedere a questo sito.";
+$lang['ac_retour'] = 'Non desidero attivare questo genere di contenuti e desitero ritornare alla pagina precedente';
+$lang['ac_retour_b'] = 'Non desidero attivare questo genere di contenuti';
+$lang['ac_retour_c'] = 'Desitero ritornare alla pagina precedente';
 
-$lang['ac_charte_user_not'] = 'Veuillez sélectionner votre âge !';
-$lang['ac_charte_user_def'] = 'changer votre statut';
-$lang['ac_not_def'] = 'Cette étape est indispensable mais ne vous sera plus demandé par la suite';
-$lang['ac_def'] = 'Voulez-vous changez votre status ?';
+$lang['ac_charte_user_not'] = 'Selezionare l\'età !';
+$lang['ac_charte_user_def'] = 'cambiare il vostro statuto';
+$lang['ac_not_def'] = 'Questa operazione è indispensabile ma, in seguito, non vi sarà più richiesta';
+$lang['ac_def'] = 'Volete cambiare statuto ?';
 
 ?>
