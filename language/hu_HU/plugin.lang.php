a8183f9758f53b1869ec7d166012c18869a92b2b
samli <samli@piwigo.org>
2010-02-28 14:48:53 +0000

    [extensions]- adult_content add hu_HU
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@4993 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+$lang['ac_title_menu'] = 'Figyelem';
+$lang['ac_title_menu_statut'] ='Információ';
+$lang['ac_text'] = 'A galéria felnőtteknek és/vagy érett személyeknek szóló képeket tartalmaz.';
+$lang['ac_lien'] = 'védelem eltávolítása';
+$lang['ac_identif'] = 'vagy belépés';
+$lang['ac_fermer'] = 'vagy bezárás';
+
+$lang['ac_title_fermer'] = 'tartalom szűrés';
+
+$lang['ac_user_text_18'] = 'Ön elmúlt 18 éves';
+$lang['ac_user_text_16'] = 'Ön elmúlt 16 vagy 17 éves';
+$lang['ac_logout'] = 'vagy más módon, kattintson ide';
+
+$lang['ac_user_no'] = "nem engedélyezem a tartalmat ";
+$lang['ac_user_no_s'] = "Tartalom szűrés";
+$lang['ac_statut'] ='Az Ön státusza  :';
+
+$lang['ac_button_activ_18'] = 'Engedélyezem a pornográf tartalmat';
+$lang['ac_button_activ_16'] = 'Engedélyezem az erotikus tartalmat';
+$lang['ac_text_charte'] = "Az oldal felnőtteknek szóló pornográf ill. erotikus képeket, szövegeket, videokat és egyéb tartalmat jelenít meg. A tartalom megtekintése csak felnőtt felhasználóink számára engedélyezett. A továbbiakban az erre vonatkozó nyilatkozatot a megfelelő gombra történő kattintással teszi meg. A honlap üzemeltetője nem vállal felelősséget a valótlan nyilatkozatokból származó nemkívánatos tartalmak megtekintéséért. </p></p>
+<p><B>Nyilatkozat:</B>";
+$lang['ac_text_activ_18'] = "  <u>... és garantálom, hogy </u>: <br>
+- <B>Elmúltam 18 éves és nagykorú vagyok </B> az országom törvényei szerint. <br>
+- Megértettem a figyelmeztetést, hogy az oldalon pornográf és erotikus tartalom található. <br>
+- Az országom törvényei engedélyezik, hogy megnézzem a tartalmat.";
+$lang['ac_text_activ_16'] = "<u> ... és garantálom, hogy </u>: <br>
+        - <B>Elmúltam 16 vagy 17 éves.</B> <br>
+- Megértettem a figyelmeztetést, hogy az oldalon pornográf és erotikus tartalom található. <br>
+- Az országom törvényei engedélyezik, hogy megnézzem a tartalmat.";
+$lang['ac_retour'] = 'Nem engedélyezem a tartalmat - visszatérés az előző oldalra';
+$lang['ac_retour_b'] = 'Nem engedélyezem a tartalmat';
+$lang['ac_retour_c'] = 'Visszatérés az előző oldalra';
+
+$lang['ac_charte_user_not'] = 'Kérem vállassza ki a kort!';
+$lang['ac_charte_user_def'] = 'státusz megváltoztatása';
+$lang['ac_not_def'] = 'Ez lényeges jog, többször nem kérdezi meg';
+$lang['ac_def'] = 'Szeretné megváltoztatni a státuszát ?';
+?>
