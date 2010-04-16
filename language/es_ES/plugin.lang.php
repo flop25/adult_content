a2e0826da228a3a327ee8ecc2fce1569506b1774
jpr928 <jpr928@piwigo.org>
2010-04-16 17:26:16 +0000

    update ES_es language
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@5883 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -1,10 +1,10 @@
 <?php
 $lang['ac_title_menu'] = 'Atención';
 $lang['ac_title_menu_statut'] ='Información';
-$lang['ac_text'] = 'Esta galería contiene imágenes que se addressant a personas maduras y\o mayores';
+$lang['ac_text'] = 'Esta galería contiene imágenes reservadas a personas maduras y\o mayores';
 $lang['ac_lien'] = 'Quitar la protección';
 $lang['ac_identif'] = 'O identificarse';
-$lang['ac_fermer'] = 'o más fijar';
+$lang['ac_fermer'] = 'o no visualizar más';
 
 $lang['ac_title_fermer'] = 'contenido filtrado';
 
@@ -12,29 +12,29 @@ $lang['ac_user_text_18'] = 'Usted tiene más de 18 años';
 $lang['ac_user_text_16'] = 'Usted tiene 16 o 17 años';
 $lang['ac_logout'] = 'O sino haga clic aquí';
 
-$lang['ac_user_no'] = "Usted no deseó acelerar este género de contenido";
-$lang['ac_user_no_s'] = "Filtro acelerado";
+$lang['ac_user_no'] = "Usted no deseó activar este género de contenido";
+$lang['ac_user_no_s'] = "Filtro activado";
 $lang['ac_statut'] ='Su estatuto :';
 
-$lang['ac_button_activ_18'] = 'Acelerar el contenido pornográfico';
-$lang['ac_button_activ_16'] = 'Acelerar el contenido erótico';
-$lang['ac_text_charte'] = "Usted entra sobre una sitio cuyo contenido visual a carácter erótico y pornográfico es destinado exclusivamente a un público advertido y puede presentar contenidos a carácter erótico. Esta sitio se dirige a las personas mayores y consentidores. Esta sitio presente declina toda responsabilidad en cuanto a la utilización que será hecha contenidos y servicios de esta sitio por toda menor o superior persona susceptible de ser chocada por ésta. Toda persona que acepta entrar en el seno de esta sitio estrictamente reservada para un público advertido descarga esta sitio presente de todas las consecuencias de su consulta. </p>
-<p> Si violo uno de estos empeños o en caso de falsas declaración, podré estar en violación con las leyes de mi país , y <B>asumiré entera responsabilidad de mis actos</B>. Declaro plenamente haber sido informado sobre el contenido propuesto y renuncio a todas persecución judiciales contra el abastecedor de acceso y los editores de esta sitio. Leí atentamente los párrafos más arriba y firmo electrónicamente mi acuerdo con lo que precede haciendo clic en el botón correspondiente a mi edad.";
+$lang['ac_button_activ_18'] = 'Activar el contenido pornográfico';
+$lang['ac_button_activ_16'] = 'Activar el contenido erótico';
+$lang['ac_text_charte'] = "Usted entra en una sitio cuyo contenido visual a carácter erótico y pornográfico es destinado exclusivamente a un público advertido y puede presentar contenidos a carácter erótico. Este sitio se dirige a las personas mayores. Este sitio presente declina toda responsabilidad en cuanto a la utilización de los contenidos y servicios de este sitio por parte de toda persona menor o mayor  susceptible de ser chocada por éste. Toda persona que acepta entrar en este sitio estrictamente reservada a un público advertido sera responsable de todas las consecuencias de su consulta. </p>
+<p> Si violo uno de estos empeños o en caso de falsas declaración, podré estar en violación con las leyes de mi país , y <B>asumiré entera responsabilidad de mis actos</B>. Declaro plenamente haber sido informado sobre el contenido propuesto y renuncio a toda persecución judicial contra el provedor de acceso y los editores de este sitio. Leí atentamente los párrafos más arriba y firmo electrónicamente mi acuerdo con lo que precede haciendo clic en el botón correspondiente a mi edad.";
 $lang['ac_text_activ_18'] = "  <u>... y certifico sobre el honor que </u>: <br>
-- <B>Tengo más de 18 años y soy superior</B> según la ley vigente en mi país. <br>
-- Soy advertido que el programa propone imágenes a carácter pornográfico y erótico. <br>
+- <B>Tengo más de 18 años y soy mayor</B> según la ley vigente en mi país. <br>
+- Estoy advertido que el programa propone imágenes a carácter pornográfico y erótico. <br>
 - Las leyes de mi país me autorizan a acceder a esta sitio.";
 $lang['ac_text_activ_16'] = "<u> ... y certifico sobre el honor que </u>: <br>
         - <B>Tengo 16 o 17 años.</B> <br>
-- Soy advertido que esta sitio propone imágenes a carácter erótico. <br>
+- Estoy advertido que este sitio propone imágenes a carácter erótico. <br>
 - Las leyes de mi país me autorizan a acceder a esta sitio.";
-$lang['ac_retour'] = 'No deseo acelerar este género de contenido y regreso sobre la página precedente';
-$lang['ac_retour_b'] = 'No deseo acelerar este género de contenido';
-$lang['ac_retour_c'] = 'Regreso sobre la página precedente';
+$lang['ac_retour'] = 'No deseo activar este género de contenido y regreso sobre la página precedente';
+$lang['ac_retour_b'] = 'No deseo activar este género de contenido';
+$lang['ac_retour_c'] = 'Regreso a la página precedente';
 
 $lang['ac_charte_user_not'] = 'Por favor, seleccione su edad !';
 $lang['ac_charte_user_def'] = 'cambiar su estatuto';
 $lang['ac_not_def'] = 'Esta etapa es indispensable pero no le será pedida más más tarde';
-$lang['ac_def'] = 'Usted quiere cambie su status ?';
+$lang['ac_def'] = 'Usted quiere cambiar su estatuto ?';
 
 ?>
