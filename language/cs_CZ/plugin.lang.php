12105d4f202da10c9bf931b2593a0af1416591e3
pab <pab@piwigo.org>
2010-11-03 15:52:59 +0000

    czech translation for adult_content.
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@7603 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+$lang['ac_title_menu'] = 'Varování';
+$lang['ac_title_menu_statut'] ='Informace';
+$lang['ac_text'] = 'Tato galerie má obsah vhodný pouze pro dospělé.';
+$lang['ac_lien'] = 'zrušit tuto ochranu';
+$lang['ac_identif'] = 'nebo se přihlásit';
+$lang['ac_fermer'] = 'nebo konec';
+
+$lang['ac_title_fermer'] = 'obsah je filtrován';
+
+$lang['ac_user_text_18'] = 'Jste starší 18-ti let';
+$lang['ac_user_text_16'] = 'je Vám mezi 16 a 17 roky';
+$lang['ac_logout'] = 'jinak klikněte zde';
+
+$lang['ac_user_no'] = "nechcete procházet tento druh obsahu";
+$lang['ac_user_no_s'] = "Obsah je filtrován";
+$lang['ac_statut'] ='Vaše rozhodnutí  :';
+
+$lang['ac_button_activ_18'] = 'pornografický obsah povolen';
+$lang['ac_button_activ_16'] = 'erotický obsah povolen';
+$lang['ac_text_charte'] = "Vstupujete na místo s vizuálním obsahem erotického a pornografického charakteru, který je určen výhradně pro plnoleté. 
+Místo se zříká veškeré zodpovědnosti za využití tohoto obsahu. Každý musí před vstupem souhlasit se vstupem na toto místo a prohlásit, že je plnoletý a vstupem na toto místo neporušuje zákony své země. Timto přebírá veškerou zodpovědnost. </p><p>Deklaruji, že jsem byl kompletně informován o obsahu a zříkám se jakýchkoliv akcí vůči poskytovateli a administrátorům tohoto místa. Přečetl jsem pozorně odstavce výše a elektronicky potvrzuji můj souhlas stikem tlačítka odpovídajícímu mému věku.";
+$lang['ac_text_activ_18'] = "  <u>... a čestně prohlašuji, že </u>: <br>
+- <B>jsem starší než 18 let a jsem plnoletý </B> podle zákonů v mé vlasti. <br>
+- byl jsem varován že tato galerie obsahuje pornografický a erotický obsah. <br>
+- zákony v mé zemi mi umožňují prohlížet takovýto obsah.";
+$lang['ac_text_activ_16'] = "<u> ... a čestně prohlašuji, že  </u>: <br>
+        - <B> je mi mezi 16 a 17 roky.</B> <br>
+- byl jsem varován že tato galerie obsahuje pornografický a erotický obsah. <br>
+- zákony v mé zemi mi umožňují prohlížet takovýto obsah.";
+$lang['ac_retour'] = 'Nechci otevírat takovýto druh obsahu a proto se vracím na předcházející stránku';
+$lang['ac_retour_b'] = 'Nechci tento druh obsahu';
+$lang['ac_retour_c'] = 'Vracím se na předchozí stránku';
+
+$lang['ac_charte_user_not'] = 'Prosím zvolte svůj věk!';
+$lang['ac_charte_user_def'] = 'změňte svůj status';
+$lang['ac_not_def'] = 'Tato část je důležitá ale již nebudete znovu tázán.';
+$lang['ac_def'] = 'Chcete změnit svůj stav ?';
+?>
