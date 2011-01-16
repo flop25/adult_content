8eb7d59a61f5db9e38e310dbcd7a86bc9a7b7543
ddtddt <ddtddt@piwigo.org>
2011-01-16 19:44:36 +0000

    [extensions] - adult_content - add lv_LV (Latvian) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@8709 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+$lang['ac_title_menu'] = 'Brīdinājums';
+$lang['ac_title_menu_statut'] ='Informācija';
+$lang['ac_text'] = 'Šajā galerijā ir attēli domāti nobriedušām un/vai pieaugušām personām.';
+$lang['ac_lien'] = 'noņemt šo aizsardzību';
+$lang['ac_identif'] = 'vai ielogoties';
+$lang['ac_fermer'] = 'vai aizvērt';
+
+$lang['ac_title_fermer'] = 'saturs filtrēts';
+
+$lang['ac_user_text_18'] = 'Jums ir vairāk par 18 gadiem';
+$lang['ac_user_text_16'] = 'Jums ir 16 vai 17 gadu';
+$lang['ac_logout'] = 'vai pretējā gadījumā klikšķiniet šeit';
+
+$lang['ac_user_no'] = "jūs nevēlaties aktivēt šada veida saturu ";
+$lang['ac_user_no_s'] = "Saturs filtrēts";
+$lang['ac_statut'] ='Jūsu statuss :';
+
+$lang['ac_button_activ_18'] = 'Aktivēt pornogrāfiska rakstura saturu';
+$lang['ac_button_activ_16'] = 'Aktivēt erotiska rakstura saturu';
+$lang['ac_text_charte'] = "Jūs esat iegājis vietnē, kuras vizuālais saturs ir pornogrāfiska un erotiska rakstura un domāts tikai par to brīdinātai auditorijai. Šī vietne adresēta personām, kas to vēlas. Šāda veida satura pārstāvētā vietne noņem no sevis jebkāda veida atbildību par to, ka tā ar savu saturu varētu šokēt kādu personu vai personu grupu. Ikviena persona, kas piekritusi ieiet šajā vietnē, domātai tikai brīdinātai publikai, ielādējot šīs vietnes saturu, nes pilnu atbildību saskaņā ar vietnes noteikumiem.</p>
+<p>Gadījumā, ja es pārkāpšu kādu no vienošanās punktiem vai būšu devis nepatiesas ziņas, man par to būs jānes atbildība, kādu par to paredz manas valsts likumdošana un<B> jāuzņemas atbildība par visiem izdarītajiem pārkāpumiem</B>. Es apliecinu, ka esmu pilnībā informēts par vietnes piedāvāto saturu un neizvirzīšu pret piekļuves nodrošinātāju un šīs vietnes administrātoriem nekāda veida juridiskas prasības. Esmu uzmanīgi izlasījis visus augšminētos punktus un elektroniski parakstījis vienošanos, noklikšķinot attiecīgo pogu, kas atbilst manam vecumam.";
+$lang['ac_text_activ_18'] = " <u>...un es, dodot goda vārdu, apliecinu, ka </u>:
+<br> - <B>Man ir vairāk par 18 gadiem un es ievēroju </B> savas valsts likumdošanu.
+<br> - Es esmu brīdināts, ka šī vietne piedāvā pornogrāfiska un erotiska rakstura saturu.
+<br> - Manas valsts likumdošana atļauj man piekļūt šai vietnei.";
+$lang['ac_text_activ_16'] = "<u> ... un es, dodot goda vārdu, apliecinu, ka </u>:
+<br> - <B>Man ir 16 vai 17 gadu.</B>
+<br> - Es esmu brīdināts, ka šī vietne piedāvā pornogrāfiska un erotiska rakstura saturu.
+<br> - Manas valsts likumdošana atļauj man piekļūt šai vietnei.";
+$lang['ac_retour'] = 'Es nevēlos aktivēt šāda veida saturu un atgriežos iepriekšējā lapā';
+$lang['ac_retour_b'] = 'Nevēlos šāda veida saturu';
+$lang['ac_retour_c'] = 'Atgriežos iepriekšējā lapā';
+
+
+$lang['ac_charte_user_not'] = 'Lūdzu izvēlaties savu vecumu!';
+$lang['ac_charte_user_def'] = 'izmainiet savu statusu';
+$lang['ac_not_def'] = 'Šis posms ir svarīgs, bet turpmāk vairs jums netiks prasīts';
+$lang['ac_def'] = 'Vai vēlaties izmainīt savu statusu ?'; 
+?>
