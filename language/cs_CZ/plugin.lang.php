4d1fd68eb6b78f5a89a11d2c90443bec4b1694db
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-25 05:00:28 +0000

    [adult_content] Update cs_CZ, thanks to : sichr
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@14381 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/cs_CZ/plugin.lang.php
+++ language/cs_CZ/plugin.lang.php
@@ -14,12 +14,12 @@ $lang['ac_logout'] = 'jinak klikněte zde';
 
 $lang['ac_user_no'] = "nechcete procházet tento druh obsahu";
 $lang['ac_user_no_s'] = "Obsah je filtrován";
-$lang['ac_statut'] ='Vaše rozhodnutí  :';
+$lang['ac_statut'] = 'Vaše rozhodnutí:';
 
 $lang['ac_button_activ_18'] = 'pornografický obsah povolen';
 $lang['ac_button_activ_16'] = 'erotický obsah povolen';
-$lang['ac_text_charte'] = "Vstupujete na místo s vizuálním obsahem erotického a pornografického charakteru, který je určen výhradně pro plnoleté. 
-Místo se zříká veškeré zodpovědnosti za využití tohoto obsahu. Každý musí před vstupem souhlasit se vstupem na toto místo a prohlásit, že je plnoletý a vstupem na toto místo neporušuje zákony své země. Timto přebírá veškerou zodpovědnost. </p><p>Deklaruji, že jsem byl kompletně informován o obsahu a zříkám se jakýchkoliv akcí vůči poskytovateli a administrátorům tohoto místa. Přečetl jsem pozorně odstavce výše a elektronicky potvrzuji můj souhlas stikem tlačítka odpovídajícímu mému věku.";
+$lang['ac_text_charte'] = 'Vstupujete na místo s vizuálním obsahem erotického a pornografického charakteru, který je určen výhradně pro plnoleté. 
+Provozovatel se zříká veškeré zodpovědnosti za využití tohoto obsahu. Každý musí před vstupem souhlasit se vstupem na toto místo a prohlásit, že je plnoletý a vstupem na toto místo neporušuje zákony své země. Timto přebírá veškerou zodpovědnost. </p><p>Deklaruji, že jsem byl kompletně informován o obsahu a zříkám se jakýchkoliv akcí vůči poskytovateli a administrátorům tohoto místa. Přečetl jsem pozorně odstavce výše a elektronicky potvrzuji můj souhlas stikem tlačítka odpovídajícímu mému věku.';
 $lang['ac_text_activ_18'] = "  <u>... a čestně prohlašuji, že </u>: <br>
 - <B>jsem starší než 18 let a jsem plnoletý </B> podle zákonů v mé vlasti. <br>
 - byl jsem varován že tato galerie obsahuje pornografický a erotický obsah. <br>
@@ -33,8 +33,19 @@ $lang['ac_retour_b'] = 'Nechci tento druh obsahu';
 $lang['ac_retour_c'] = 'Vracím se na předchozí stránku';
 
 $lang['ac_charte_user_not'] = 'Prosím zvolte svůj věk!';
-$lang['ac_charte_user_def'] = 'změňte svůj status';
+$lang['ac_charte_user_def'] = 'jinak klikněte zde';
 $lang['ac_not_def'] = 'Tato část je důležitá ale již nebudete znovu tázán.';
-$lang['ac_def'] = 'Chcete změnit svůj stav ?';
+$lang['ac_def'] = 'Chcete změnit svůj stav?';
 
+$lang['ad_c_block_on_index_error_2'] = 'Vyžaduje PWG_Stuffs ve verzi alespoň 2.1.l';
+$lang['ad_c_block_on_index_error'] = 'Momentálně není k dispozici';
+$lang['ad_c_block_on_index'] = 'Zobrazit leták na hlavní stránce? (Vyžaduje plugin PWG_Stuffs)';
+$lang['ad_c_linktolang'] = 'Jděte do jazykového souboru Adult Content (nutno mít povolený LocalFiles Editor)';
+$lang['ad_c_manage_both'] = 'erotický a pornografický obsah';
+$lang['ad_c_manage_eroticonly'] = 'pouze erotický obsah';
+$lang['ad_c_manage_pornonly'] = 'pouze pornografický obsah';
+$lang['ad_c_manage_what'] = 'Zvolte jaký druh obsahu má plugin spravovat:';
+$lang['ad_c_menublock_for_guest'] = 'Povolit neregistrovaným uživatelům zvolit si věk?';
+$lang['ad_c_msg_admin_1'] = 'Nastavení pluginu bylo úspěšně aktualizováno!';
+$lang['ad_c_send'] = 'Uložit změny';
 ?>
