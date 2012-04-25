56b86477a6d2790cd951662ae11b7ec83900c6cf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-25 11:50:55 +0000

    [adult_content] Update de_DE, thanks to : Joergen
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@14404 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ac_button_activ_16'] = 'Aktivieren Sie erotische Inhalte';
+$lang['ac_button_activ_18'] = 'Aktivieren Sie pornographische Inhalte';
+$lang['ac_charte_user_def'] = 'andernfalls klicken Sie hier ';
+$lang['ac_charte_user_not'] = 'Bitte wählen Sie ihr alter';
+$lang['ac_def'] = 'Möchten Sie ihren Status ändern?';
+$lang['ac_fermer'] = 'oder schließe ';
+$lang['ac_identif'] = 'oder Log dich ein';
+$lang['ac_lien'] = 'Entferne diesen Schutz';
+$lang['ac_logout'] = 'andernfalls klicken Sie hier ';
+$lang['ac_retour_b'] = 'Ich möchte diese Art von Inhalten nicht sehen';
+$lang['ac_retour'] = 'Ich möchte diese Art von Inhalten nicht aktivieren und zur vorherigen Seite zurück kehren';
+$lang['ac_statut'] = 'Ihr Status';
+$lang['ac_text'] = 'Diese Galerie enhält Bilder für reife Personen und / oder Erwachsene ';
+$lang['ac_title_fermer'] = 'Inhalt gefiltert';
+$lang['ac_title_menu_statut'] = 'Information';
+$lang['ac_title_menu'] = 'Warnung';
+$lang['ac_user_no_s'] = 'Inhalt gefiltert';
+$lang['ac_user_no'] = 'Sie möchten nicht, diese Art von Inhalt aktivieren';
+$lang['ac_user_text_16'] = 'Sie sind 16 oder 17 Jahre alt';
+$lang['ac_user_text_18'] = 'Sie sind älter als 18 Jahre';
+$lang['ad_c_block_on_index_error'] = 'Zur Zeit nicht verfügbar';
+$lang['ad_c_menublock_for_guest'] = 'Erlaube unregistrierten Benutzern das Alter auszuwählen?';
+$lang['ad_c_send'] = 'Änderungen speichern';
+?>
