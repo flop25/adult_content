59f43638c24500296e8b0b171a977a6652fb479f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-26 15:17:15 +0000

    [adult_content] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@14443 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ac_button_activ_16'] = 'Активізуйте еротичний вміст';
+$lang['ac_button_activ_18'] = 'Активізуйте порнографічний вміст';
+$lang['ac_fermer'] = 'або закрити';
+$lang['ac_identif'] = 'або увійти';
+$lang['ac_lien'] = 'видаліть цей захист';
+$lang['ac_logout'] = 'або в іншому випадку натисніть тут';
+$lang['ac_title_fermer'] = 'вміст нефільтрований';
+$lang['ac_title_menu_statut'] = 'Інформація';
+$lang['ac_title_menu'] = 'Попередження';
+$lang['ac_user_no_s'] = 'Вміст нефільтрований';
+$lang['ac_user_text_16'] = 'Вам 16 або 17 років';
+$lang['ac_user_text_18'] = 'Вам більш ніж 18 років';
+$lang['ad_c_block_on_index_error'] = 'Не доступний зараз';
+$lang['ad_c_linktolang'] = 'Перейти до мовного файлу із вмістом для дорослих (необхідно ввімкнути LocalFiles Editor)';
+$lang['ad_c_manage_both'] = 'еротичний і порнографічний вміст';
+$lang['ad_c_manage_eroticonly'] = 'лише з еротичні вміст';
+$lang['ad_c_manage_pornonly'] = 'тільки порнографічний вміст';
+$lang['ad_c_msg_admin_1'] = 'Конфігурація плаґіна успішно оновлена!';
+$lang['ad_c_send'] = 'Зберегти зміни';
+?>
