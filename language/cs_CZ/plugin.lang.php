705b99163ccac93460f9194e93c8dde63db9f298
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-27 05:12:55 +0000

    [adult_content] Update cs_CZ, thanks to : sichr
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@14461 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/cs_CZ/plugin.lang.php
+++ language/cs_CZ/plugin.lang.php
@@ -48,4 +48,41 @@ $lang['ad_c_manage_what'] = 'Zvolte jaký druh obsahu má plugin spravovat:';
 $lang['ad_c_menublock_for_guest'] = 'Povolit neregistrovaným uživatelům zvolit si věk?';
 $lang['ad_c_msg_admin_1'] = 'Nastavení pluginu bylo úspěšně aktualizováno!';
 $lang['ad_c_send'] = 'Uložit změny';
+$lang['ad_c_howitworks'] = 'Princip tohoto pluginu je v jeho pomoc při zacházením s obsahem pro dospělé (může být taktéž využit pro obsah týkající se násilí). Plugin spravuje návštěvníky a/nebo uživatele dle vašeho přání, dle parametrů, které nastavíte.<br />
+<br />
+1 - Jako admin musíte umístit nevhodný obsah do privátní galerie, nebo alba (plugin neumožňuje spravovat jednotlivé fotografie, aspoň zatím ne). Pak přiřaďte galerii nebo album do jedné ze dvou skupin \'18\' nebo \'16-17\' (vytvořených pluginem) takto:<br />
+<br />
+- uživatelé ze skupiny \'18\' mají dovoleno zobrazit nevhodný obsah určemý pro starší 18-ti let a nezávadný obsah (ten ale obecně bývá veřejný)
+<br /><br />
+- uživatelé ze skupiny \'16-17\' mají dovoleno zobrazit nevhodný obsah určemý pro starší 16-ti a mladší 18-ti let a nezávadný obsah (ten ale obecně bývá veřejný)
+<br /><br />
+- uživatelé ze skupiny \'nic\' mají dovoleno zobrazovat pouze nezávadný obsah (ten ale obecně bývá veřejný)
+
+<br /><br />
+
+2 - Funkce pluginu<br />
+2.1 - Správa uživatelů
+<br />
+<br />
+Pokud je již uživatel zaznamenán v administraci pluginu, plugin ukáže jen malé políčko poukazující na to, že uživatel musí zadat svůj věk. Jakmile klikne, zobrazí se stránka, která upozorní, že jde o nevhodný obsah s možností zadat uživatelův věk (Charte_user.php).
+
+Jakmile u6ivatel zadá svůj věk, je přesměrován zpět (Charte_user.php).
+
+Jednoduše plugin přiřadí uživatele do jedné ze tří skupin \'18\', \'16-17\', \'nic\' podle zadaného věku. Uživatel může kdykoliv změnit svůj zadaný věk v blokovém menu Adult_Content, pokud to není zakázáno v administrační části Piwigo (pozor, přímý link na charte_user.php zůstává stále funkční): pokud si přejete, aby uživatel nemohl měnit svůj věk, potom je tento plugin nevhodný a použijte správu práv na úrovni skupin, nebo samotných fotek.
+<br/>
+<br/>
+2.2 - Správa návštěvníků<br/>
+<br/>
+Pokud je aktivní volba \'Povolit neregistrovaným uživatelům zvolit si věk?\', pak plugin bude spravovat i neregistrované návštěvníky. * Zobrazuje blokové menu indikující, že obsah galerie je \'filtrovaný\' a pokud chce zrušit tento filtr musí zadat svůj věk.
+Následně je přesměrován na stránku pro zadání věku (chartre.php).
+
+Návštěvník je pak zapsán jako jeden ze dvou uživatelů vytvořených pluginem: \'18\' patřící do skupiny \'18\' a nebo \'16\' ze skupiny \'16-17\'. Tyto dva uživatelské účty mohou být modifikovány pouze administrátorem, nikoliv uživatelem.
+
+Skrze tyto dva účty závislé na věku návštěvníků můžete ovlivnit co se komu zobrazí.
+<br />
+<br />
+
+2.3 - Možné adaptace<br/>
+<br/>
+Tento plugin může být adaptován pro zobrazování obsahu obsahujícího násilí tak, že změníte jazykové soubory tohoto pluginu (použijte plugin Local Files Editor pro větší jednoduchost). Věkové limity mohou taktéž být změněny touto cestou.';
 ?>
