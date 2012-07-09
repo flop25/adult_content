250daf8b35ee2c0d5a140f813a8a6e4beb6bf052
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-09 12:14:34 +0000

    [adult_content] Add pl_PL, thanks to : T00kie
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@16540 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ac_button_activ_16'] = 'Uaktywnij zawartość erotyczną';
+$lang['ac_button_activ_18'] = 'Uaktywnij zawartość pornograficzną';
+$lang['ac_charte_user_def'] = 'lub kliknij tutaj';
+$lang['ac_charte_user_not'] = 'Podaj swój wiek!';
+$lang['ac_def'] = 'Czy chcesz zmienić status?';
+$lang['ac_fermer'] = 'lub zamknąć';
+$lang['ac_identif'] = 'lub zalogować';
+$lang['ac_lien'] = 'usuń to zabezpieczenie';
+$lang['ac_logout'] = 'lub kliknij tutaj';
+$lang['ac_not_def'] = 'Ten etap jest kluczowy ale nie będzie już powtarzany';
+$lang['ac_retour_b'] = 'Nie chcę widzieć tego rodzaju zawartości';
+$lang['ac_retour_c'] = 'Wracam na poprzednią stronę';
+$lang['ac_retour'] = 'Nie chcę aktywować tego rodzaju zawartości, wróć na poprzednią stronę';
+$lang['ac_statut'] = 'Oświadczenie';
+$lang['ac_text'] = 'Ta galeria zawiera zdjęcia tylko dla osób pełnoletnich';
+$lang['ac_title_fermer'] = 'zawartość wyfiltrowana';
+$lang['ac_title_menu_statut'] = 'Informacja';
+$lang['ac_title_menu'] = 'Ostrzeżenie';
+$lang['ac_user_no_s'] = 'Zawartość wyfiltrowana';
+$lang['ac_user_no'] = 'Nie chciałeś aktywować tego rodzaju zawartości';
+$lang['ac_user_text_16'] = 'Masz 16 lub 17 lat';
+$lang['ac_user_text_18'] = 'Masz więcej niż 18 lat';
+$lang['ad_c_block_on_index_error_2'] = 'Minimalna wymagana wersja PWG_Stuffs to 2.1';
+$lang['ad_c_block_on_index_error'] = 'Aktualnie niedostępne';
+$lang['ad_c_linktolang'] = 'Idź do pliku language tego pluginu (wymagany LocalFiles Editor)';
+$lang['ad_c_manage_both'] = 'zawartość erotyczna i pornograficzna';
+$lang['ad_c_manage_eroticonly'] = 'zawartość tylko erotyczna';
+$lang['ad_c_manage_pornonly'] = 'zawartość tylko pornograficzna';
+$lang['ad_c_manage_what'] = 'Wybierz rodzaj zawartości, którą chcesz zarządzać tym pluginem';
+$lang['ad_c_menublock_for_guest'] = 'Pozwalać niezarejestrowanym użytkownikom aby wybierali wiek?';
+$lang['ad_c_msg_admin_1'] = 'Konfiguracja pluginu zaktualizowana pomyślnie';
+$lang['ad_c_send'] = 'Zapisz zmiany';
+?>
