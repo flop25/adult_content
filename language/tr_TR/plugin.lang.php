b301723abe6ad45f8a16a408d566b96a1d30899d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-13 21:50:56 +0000

    [adult_content] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@16707 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,56 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ac_button_activ_16'] = 'Erotik içeriği etkinleştir';
+$lang['ac_button_activ_18'] = 'Pornografik içeriği etkinleştir';
+$lang['ac_charte_user_def'] = 'veya ayrıca buraya tıkla';
+$lang['ac_charte_user_not'] = 'Lütfen yaşınızı seçin!';
+$lang['ac_def'] = 'Durumunuzu değiştirmek istiyor musunuz ?';
+$lang['ac_fermer'] = 'veya kapat';
+$lang['ac_identif'] = 'veya giriş yap';
+$lang['ac_lien'] = 'bu korumayı kaldır';
+$lang['ac_logout'] = 'veya ayrıca buraya tıkla';
+$lang['ac_not_def'] = 'Bu adım gereklidir ancak bir daha size sorulmayacaktır';
+$lang['ac_retour_b'] = 'Bu tip içeriği görmek istemiyorum';
+$lang['ac_retour_c'] = 'Önceki sayfaya geri döndüm';
+$lang['ac_retour'] = 'Bu tip içeriği etkinleştirmeden önceki sayfaya dönmek istiyorum';
+$lang['ac_statut'] = 'Tüzüğünüz :';
+$lang['ac_text'] = 'Bu galeri yetişkinler için görüntüler içerir.';
+$lang['ac_title_fermer'] = 'içerik filtrelendi';
+$lang['ac_title_menu_statut'] = 'Bilgi';
+$lang['ac_title_menu'] = 'Uyarı';
+$lang['ac_user_no_s'] = 'İçerik filtrelendi';
+$lang['ac_user_no'] = 'bu tip içeriği etkinleştirmek istemediniz';
+$lang['ac_user_text_16'] = 'Yaşınız 16 veya 17 dir';
+$lang['ac_user_text_18'] = 'Yaşınız 18 \'den büyük';
+$lang['ad_c_block_on_index_error_2'] = 'PWG_Stuffs ekletisi en az sürüm 2.1.l gerektirir';
+$lang['ad_c_block_on_index_error'] = 'Şu an için kullanılamaz';
+$lang['ad_c_block_on_index'] = 'Ana sayfada bir ilan görüntülensin mi? (PWG_Stuffs ekletisi gerektirir)';
+$lang['ad_c_linktolang'] = 'Adult Content eklentisi dil dosyasına gidiniz (LocalFiles düzenleyicisinin etkin olması gerekir)';
+$lang['ad_c_manage_both'] = 'erotik ve pornografik içerik';
+$lang['ad_c_manage_eroticonly'] = 'sadece erotik içerik';
+$lang['ad_c_manage_pornonly'] = 'sadece pornografik içerik';
+$lang['ad_c_manage_what'] = 'Eklentinin yöneteceği içerik tipini seçin:';
+$lang['ad_c_menublock_for_guest'] = 'Kayıtlı olmayan kullanıcıların yaşlarını seçmelerine izin ver?';
+$lang['ad_c_msg_admin_1'] = 'Eklenti yapılandırması başarıyla kaydedildi!';
+$lang['ad_c_send'] = 'Değişiklikleri Kaydet';
+?>
