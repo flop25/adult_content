f99da044fc690870308f70827ea4bda4e2e0defd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-29 16:57:37 +0000

    [adult_content] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@17203 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/tr_TR/plugin.lang.php
+++ language/tr_TR/plugin.lang.php
@@ -63,4 +63,77 @@ $lang['ac_text_activ_18'] = '  <u>... ve ben şerefim üzerine yemin ederim ki <
 - Ülkemdeki kanunlar bana bu siteye erişim hakkı vermektedir.';
 $lang['ac_text_charte'] = 'Bu site kendi rızası ile erişim yapacak yetişkinlere yöneliktir.
 (translation to be continued...)';
+$lang['ad_c_howitworks'] = 'The principle of the plugin is to help in the management of mature content (it can also be
+
+adapted for violent content). To do this, the plugin manages the visitors and/or the users
+
+as you wish according to your definition of parameters of the plugin.<br />
+<br />
+1 - As administrator you must put your adult content in a private gallery or album (the
+
+plugin does not manage the rights to the level of images itself, at least not yet). Then
+
+assign the gallery or album to the two groups \'18\' and \' 16-17\' (created by the plugin)
+
+like this:<br /><br />
+
+-allow the group \'18\' to see the adult content, reserved for users over the of age of 18
+
+and normal content (but generally the normal content is often public)
+<br /><br />
+-allow the group \' 16-17\' to see the adult content reserved for users over the age of 16
+
+and normal content (generally the normal content is often public)
+<br /><br />
+- the \'nothing\' group will see the normal content (but generally the normal content is
+
+often public)
+<br />
+
+<br />
+2 - Functioning of the plugin<br />
+2.1 - The management of the users<br />
+<br />
+If the user is already recorded in the activation of the plugin, the plugin shows a small
+block pointing out that he must choose his age. Once he clicks, the page containing the message of \'adult content\' will appear and the choice of choosing their age (Charte_user.php).
+
+Once a visitor chooses his age, it is re-aimed to the page of choice of age (Charte_user.php), directly after its recording.
+
+In practice, the plugin places the user in the 3 groups, \' 18\' \' 16-17\' and \'nothing\',
+
+according to his choice of age. A user may at any time change his age via the block menu of
+
+adult_content except if it is disabled via the administration of piwigo (attention the
+
+direct link to charte_user.php continues to function all the same) : if you wish that a
+
+user cannot change his age then this plugin is useless and use directly the management of
+
+rights at the level of groups and/or images.<br />
+<br />
+2.2 - Visitor management<br />
+<br />
+If the option \'Allow visitors to non-registered to select their age ?\' is enabled, then the
+
+plugin will manage the visitors. * It displays a block menu, indicating the gallery is
+
+\'filtered\' and that if he wishes to disable the filter, he must select his age.
+It is then redirected to the page of choice of the age for visitor (chartre.php).
+
+In practice, the visitor is then recorded under one of the 2 users created by the plugin: \'
+
+18\' belonging to the group \' 18 \', and \' 16\' from the group \' 16-17 \'.  These 2 user
+
+accounts may not be modified by users, only by the administrator.
+You open and choose to change the theme according to the age of the vistor via these 2 user
+
+accounts.<br />
+<br />
+2.3 - Possible adaptations<br />
+<br />
+You can adapt this plugin for the violent content, simply by changing the files languages
+
+of the plugin (use the plugin Local Files Editor for more ease).  The age limits may also
+
+be changed in this way.';
 ?>
