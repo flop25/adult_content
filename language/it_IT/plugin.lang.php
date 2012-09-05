6c1e06a815f9310f29483e0ebf9ced5ffeee127f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-05 21:11:42 +0000

    [adult_content] Update it_IT, thanks to : virgigiole
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@17771 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -1,8 +1,8 @@
 <?php
 $lang['ac_title_menu'] = 'Attenzione';
 $lang['ac_title_menu_statut'] ='Informatione';
-$lang['ac_text'] = 'Questa galleria contiene delle foto destinate ad un pubbilico adulto';
-$lang['ac_lien'] = 'togliere la protezzione';
+$lang['ac_text'] = 'Questa galleria contiene delle foto destinate ad un pubblico adulto';
+$lang['ac_lien'] = 'togliere la protezione';
 $lang['ac_identif'] = 'o identificarsi';
 $lang['ac_fermer'] = 'o non più visualizzare';
 
@@ -10,32 +10,32 @@ $lang['ac_title_fermer'] = 'contenuto filtrato';
 
 $lang['ac_user_text_18'] = 'Avete più di 18 anni';
 $lang['ac_user_text_16'] = 'Avete 16 o 17 anni';
-$lang['ac_logout'] = 'o allora cliccare qui';
+$lang['ac_logout'] = 'oppure cliccare qui';
 
 $lang['ac_user_no'] = 'non avete desiderato attivare questo tipo di contenuti';
 $lang['ac_user_no_s'] = 'Filtro attivato';
-$lang['ac_statut'] ='Vostro statuto :';
+$lang['ac_statut'] = 'Il tuo stato :';
 
 $lang['ac_button_activ_18'] = 'Attivare il contenuto pornografico';
 $lang['ac_button_activ_16'] = 'Attivare il contenuto erotico';
-$lang['ac_text_charte'] = 'State entrando in un sito nel quale i contenuti a carattere erotico e/o pornografico sono destinati ad un pubblico adulto. Questo sito è di fatto destinato ad un pubblico maggiorenne. Il presente sito non può essere tenuto responsabile del uso che verra fatto dei contenuti e servizzi del suddetto sito. Accettando di entrare in questo sito destinato ad un pubblico adulto, assumete di fatto tutte le conseguezze legate alla sua consultazzione. </p>
-<p> Se vado al incontro di quanto dichiarato o in caso di falza dichiarazione, potrei essere in violazione con le leggi del mio paese, e <B>ne assumo l\'intera reponsabilità</B>. Dichiaro essere stato correttamente informato dei contenuti proposti e rinunciare a tutti i procedimenti nei confronti del provaider e del/degli autore/i di questo sito. Ho letto attentamente i paragrafi qui sopra e firmo elettronicamente il mio accordo, con ciò che precede, cliccando sul pulsante corrispondente alla mia età.';
-$lang['ac_text_activ_18'] = '  <u>... e certifico sul\'onore che </u>: <br>
+$lang['ac_text_charte'] = 'State entrando in un sito nel quale i contenuti a carattere erotico e/o pornografico sono destinati ad un pubblico adulto. Questo sito è di fatto destinato ad un pubblico maggiorenne. Il presente sito non può essere ritenuto responsabile dell\'uso che verrà fatto dei contenuti e servizi del suddetto sito. Accettando di entrare in questo sito destinato ad un pubblico adulto, assumete di fatto tutte le conseguenze legate alla sua consultazione. </p>
+<p> In caso di violazioni di questi impegni o in caso di falsa dichiarazione, potrei essere in violazione con le leggi del mio paese, e <B>me ne assumo l\'intera reponsabilità</B>. Dichiaro di essere stato correttamente informato dei contenuti proposti e rinunciare a tutti i procedimenti nei confronti del provider e del/degli autore/i di questo sito. Ho letto attentamente i paragrafi qui sopra e firmo elettronicamente il mio accordo, con ciò che precede, cliccando sul pulsante corrispondente alla mia età.';
+$lang['ac_text_activ_18'] = '  <u>... e garantisco sull\'onore che </u>: <br>
 - <B>ho più di 18 anni e che sono maggiorenne </B> secondo le leggi in vigore nel mio paese. <br>
 - sono al corrente che il sito propone delle foto a carattere pornografico e/o erotico. <br>
-- Le leggi del mio paese mi autorizzono ad accedere a questo sito.';
-$lang['ac_text_activ_16'] = '<u> ... e certifico sul\'onore che </u>: <br>
+- Le leggi del mio paese mi autorizzano ad accedere a questo sito.';
+$lang['ac_text_activ_16'] = '<u> ... e garantisco sull\'onore che </u>: <br>
         - <B>ho 16 o 17 anni.</B> <br>
-- sono al corrente che il sito propone delle foto a carattere erotico. <br>
-- Le leggi del mio paese mi autorizzono ad accedere a questo sito.';
-$lang['ac_retour'] = 'Non desidero attivare questo genere di contenuti e desitero ritornare alla pagina precedente';
+- sono al corrente che il sito propone delle foto a carattere pornografico ed erotico. <br>
+- Le leggi del mio paese mi autorizzano ad accedere a questo sito.';
+$lang['ac_retour'] = 'Non desidero attivare questo genere di contenuti e desidero ritornare alla pagina precedente';
 $lang['ac_retour_b'] = 'Non desidero attivare questo genere di contenuti';
 $lang['ac_retour_c'] = 'Desitero ritornare alla pagina precedente';
 
 $lang['ac_charte_user_not'] = 'Selezionare l\'età!';
 $lang['ac_charte_user_def'] = 'cambiare il vostro statuto';
 $lang['ac_not_def'] = 'Questa operazione è indispensabile ma, in seguito, non vi sarà più richiesta';
-$lang['ac_def'] = 'Volete cambiare statuto?';
+$lang['ac_def'] = 'Volete cambiare il tuo stato?';
 
 $lang['ad_c_menublock_for_guest'] = 'Permettere ai visitatori di selezionare la loro età?';
 $lang['ad_c_block_on_index'] = 'Visualizzare un flyer sulla home page ? (Necessita il plugin PWG Stuffs)';
