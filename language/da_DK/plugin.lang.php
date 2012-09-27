969934c16fafee7859a1e47bfacaa281951b85cd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-27 18:40:48 +0000

    [adult_content] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@18302 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ac_title_menu_statut'] = 'Oplysning';
+$lang['ac_user_no'] = 'du ønskede ikke at aktivere sådant indhold';
+$lang['ac_user_no_s'] = 'Indhold filtreret';
+$lang['ac_user_text_16'] = 'Du er 16 eller 17 år gammel';
+$lang['ac_user_text_18'] = 'Du er 18 år eller ældre';
+$lang['ad_c_block_on_index'] = 'Se en brochure på hjemmesiden? (Kræver plugin\'en PWG_Stuffs)';
+$lang['ad_c_block_on_index_error'] = 'Ikke tilgængelig i øjeblikket';
+$lang['ad_c_block_on_index_error_2'] = 'Kræver som minimum PWG_Stuffs version 2.1.l';
+$lang['ad_c_linktolang'] = 'Gå til Adult Contents sprogfil (kræver at LocalFiles Editor er aktiveret)';
+$lang['ad_c_manage_both'] = 'erotisk og pornografisk indhold';
+$lang['ad_c_manage_eroticonly'] = 'kun erotisk indhold';
+$lang['ad_c_manage_pornonly'] = 'kun pornografisk indhold';
+$lang['ad_c_manage_what'] = 'Vælg den form for indhold, som plugin\'en skal håndtere:';
+$lang['ad_c_menublock_for_guest'] = 'Giv ikke-registrerede brugere mulighed for at vælge deres alder?';
+$lang['ad_c_msg_admin_1'] = 'Plugin\'ens opsætning er opdateret!';
+$lang['ad_c_send'] = 'Gem ændringer';
+$lang['ac_button_activ_16'] = 'Aktiver det erotiske indhold';
+$lang['ac_button_activ_18'] = 'Aktiver det pornografiske indhold';
+$lang['ac_charte_user_def'] = 'ellers klik her';
+$lang['ac_charte_user_not'] = 'Angiv din alder!';
+$lang['ac_def'] = 'Ønsker du at ændre din status?';
+$lang['ac_fermer'] = 'eller luk';
+$lang['ac_identif'] = 'eller log på';
+$lang['ac_lien'] = 'fjern denne beskyttelse';
+$lang['ac_logout'] = 'ellers klik her';
+$lang['ac_not_def'] = 'Dette trin er afgørende, men du vil ikke blive spurgt om det igen';
+$lang['ac_retour'] = 'Jeg ønsker ikke at aktivere denne form for indhold og vender tilbage til den foregående side';
+$lang['ac_retour_b'] = 'Jeg ønsker ikke at se den form for indhold';
+$lang['ac_retour_c'] = 'Jeg vender tilbage til den foregående side';
+$lang['ac_statut'] = 'Din status:';
+$lang['ac_text'] = 'Dette galleri indeholder billeder for voksne.';
+$lang['ac_text_activ_16'] = '<u> ... og jeg garanterer, at</u>: <br>
+- <B>jeg er 16 eller 17 år gammel.</B> <br>
+- jeg er blevet advaret om, at dette websted indeholder billeder af pornografisk eller erotisk natur. <br>
+- lovene i mit land tillader, at jeg benytter dette websted.';
+$lang['ac_text_activ_18'] = '<u>... og jeg garanterer, at</u>: <br>
+- <B>jeg er ældre end 18 år og myndig</B> ifølge lovene i mit land. <br>
+- jeg er advaret om, at dette websted indeholder billeder af pornografisk eller erotisk natur. <br>
+- lovene i mit land tillader, at jeg benytter dette websted.';
+$lang['ac_text_charte'] = 'Du bevæger dig ind på et websted, hvis visuelle indhold er af erotisk eller pornografisk natur, udelukkende rettet mod folk, der er indforstået hermed og må vise erotisk indhold. Webstedet er rettet mod myndige personer, som er interesserede i indholdet. Nærværende websted fralægger sit ethvert ansvar for hvordan indholdet anvendes og de følger det kan give personer, der er følsomme over for sådant indhold. Ved at bevæge dig ind på dette websted accepterer du konsekvenserne og nærværende websted fralægges ethvert ansvar.</p>
+<p>Hvis du ikke overholder betingelserne eller afgiver falske oplysninger, overtræder du måske lovene i dit land. og <B>påtager dig det fulde ansvar for dine handlinger</B>. Jeg erklærer at være blevet udtømmende oplyst om det beskrevne indhold og frasiger retten til at lægge sag an mod webstedets indholdsleverandør eller administratorer.  Jeg har omhyggeligt læst ovennævnte afsnit og underskriver elektronisk min aftale, som træder i kraft, når jeg klikker på knappen svarende til min alder.';
+$lang['ac_title_fermer'] = 'indhold filtreret';
+$lang['ac_title_menu'] = 'Advarsel';
+?>
