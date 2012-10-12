5bbe9ff27f8e2d6dc34ebeb07e85a1214d571b92
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-12 16:10:23 +0000

    [adult_content] Update nl_NL, thanks to : cascer1
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@18625 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ac_button_activ_16'] = 'activeer de erotische inhoud';
+$lang['ac_button_activ_18'] = 'activeer de pornografische inhoud';
+$lang['ac_charte_user_def'] = 'of klik hier';
+$lang['ac_charte_user_not'] = 'selecteer uw leeftijd';
+$lang['ac_def'] = 'wilt u uw status aanpassen?';
+$lang['ac_fermer'] = 'of sluit';
+$lang['ac_identif'] = 'of log in';
+$lang['ac_lien'] = 'verwijder deze bescherming';
+$lang['ac_logout'] = 'of klik hier';
+$lang['ac_not_def'] = 'deze fase is essentieel maar zal niet meer aan u gevraagd worden';
+$lang['ac_retour'] = 'ik wil deze inhoud niet inschakelen en wil terug gaan naar de vorige pagina';
+$lang['ac_retour_b'] = 'ik wil deze soort inhoud niet zien';
+$lang['ac_retour_c'] = 'ik kom terug naar de vorige pagina';
+$lang['ac_statut'] = 'uw lichaamsbouw';
+$lang['ac_text'] = 'deze gallerij bevat afbeeldingen voor volwassen personen en/of volwassenen';
+$lang['ac_text_activ_16'] = '<u>...en ik garandeer eervol dat</u>:<br>
+ - <B>Ik 16 of 17 jaar oud ben</B><br>
+- Ik ben gewaarschuwd dat deze site pornografische of erotische inhoud kan bevatten.
+- De wetten in mijn land staan me toe om deze site te bezoeken.';
+?>
