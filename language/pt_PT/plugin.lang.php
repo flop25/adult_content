407b82ad9138efa383371ab442b9d6a4cf0583e7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-03 18:28:34 +0000

    [adult_content] Add pt_PT, thanks to : ANO
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@21135 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ad_c_menublock_for_guest'] = 'Permitir soa visitantes que selecionem a sua idade';
+$lang['ad_c_manage_both'] = 'Conteúdo erótico e pornográfico';
+$lang['ad_c_manage_eroticonly'] = 'Apenas conteúdo erótico';
+$lang['ad_c_manage_pornonly'] = 'Apenas conteúdo pornográfico';
+$lang['ad_c_msg_admin_1'] = 'Configuração da extenção atualizada';
+$lang['ad_c_send'] = 'Salvar alterações';
+$lang['ac_title_menu'] = 'Atenção';
+$lang['ac_title_menu_statut'] = 'Informação';
+$lang['ac_user_no'] = 'Não deseja ativar este tipo de conteúdo';
+$lang['ac_user_no_s'] = 'Conteúdo filtrado';
+$lang['ac_user_text_16'] = 'Tem 16 ou 17 anos de idade';
+$lang['ac_user_text_18'] = 'Tem mais de 18 anos de idade';
+$lang['ac_text_charte'] = 'Entrou num site de conteúdos visuais de cariz erótico e pornográfico exclusivamente destinados a um público avisado que, segundo a lei, é de maior idade para poder ver tais conteúdos. Este site informa os utentes e declina qualquer responsabilidade quanto às consequencias do uso que seja feito dos conteúdos e, ou serviços deste sitio por qualquer pessoa de maior ou menor idade suscetível de serem chocados pelos conteúdos. Cada pessoa concordar em entrar neste sitio estritamente reservado para um publico já avisado e conhecedor de todas as eventuais conseqüências que possam advir da sua consulta. </ p>
+<p> Se violar um destes compromissos ou em caso de declaração falsa, você pode estar violando as leis do seu país, e <B> deve assumir toda a responsabilidade de seus atos </ B>. Declara ter sido completamente informado sobre os conteúdos propostos e desistir de qualquer processo judicial contra o alojador do sitio ou seu proprietári, admistradores ou outros. declara que leu atentamente os parágrafos supra e assina eletronicamente este acordo quando clicar no botão correspondente à sua idade.';
+$lang['ac_title_fermer'] = 'Conteúdo filtrado';
+$lang['Disclaimer for the guest, with the ability to choose its age to unlock the adult content'] = 'Recusar visitante, com a possibilidade de escolher sua idade.';
+$lang['NEW! Check the Plugin PWG Stuffs to add the disclaimer on the index/album pages'] = 'NOVO! Verifique a extensão PWG Stuffs para adicionar um aviso nas páginas iniciaais dos albuns';
+$lang['No options. Just check "Allow unregistered users to select their age?" on the Adult Content administration page'] = 'Não existem opções. Basta verificar "Permitir que visitantes não registrados selecionem a sua idade?" na página da administração Conteúdo Adulto ';
+$lang['ac_button_activ_16'] = 'Ativar conteúdo erótico';
+$lang['ac_button_activ_18'] = 'Ativar conteúdo pornográfico';
+$lang['ac_charte_user_def'] = 'ou, em alternativa, clique aqui';
+$lang['ac_charte_user_not'] = 'Selecione a sua idade';
+$lang['ac_def'] = 'Deseja alterar o seu status?';
+$lang['ac_fermer'] = 'ou fechar';
+$lang['ac_identif'] = 'ou entrar';
+$lang['ac_lien'] = 'Remover esta proteção';
+$lang['ac_logout'] = 'ou, simplesmente, clique aqui';
+$lang['ac_not_def'] = 'Este passo é essêncial mas nãao lhe voltará a ser colocado';
+$lang['ac_retour'] = 'Eu não desejo ativar este tipo de conteúdos e quero voltar para a página anterior';
+$lang['ac_retour_b'] = 'Não quero ver este tipo de conteúdo';
+$lang['ac_retour_c'] = 'Voltar à página anterior';
+$lang['ac_statut'] = 'Seu Status';
+$lang['ac_text'] = 'Esta galeria contem imagfens, ou conteúdos para adultos';
+$lang['ac_text_activ_16'] = '<u>Eu... garanto por minha honra que </u>: <br>
+        - <B>Eu tenho 16 ou 17 anos </ B> <br> 
+- Eu fui avisado que este site propõe imagens com de cariz pornográfico e erótico. <br>
+- As leis do meu país autorizam-me a poder ver este site.';
+$lang['ac_text_activ_18'] = '
+<u>Eu ... garanto por minha honra que </ u>: <br>
+- <B> Tenho mais de 18 anos e sou de maior idade </ B> de acordo com a lei atual no meu país. <br>
+- Eu fui avisado que este site propõe imagens com de cariz pornográfico e erótico. <br>
+- As leis do meu país autorizam-me a poder ver este site.
+
+';
+?>
