02beb9baa4cfdbc92a5520288a2c61872b249195
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-04 16:41:35 +0000

    [adult_content] Update pt_PT, thanks to : ANO
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@21183 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pt_PT/plugin.lang.php
+++ language/pt_PT/plugin.lang.php
@@ -64,4 +64,49 @@ $lang['ac_text_activ_18'] = '
 - As leis do meu país autorizam-me a poder ver este site.
 
 ';
+$lang['ad_c_linktolang'] = 'Ir para o arquivo do idioma de conteúdo adulto (É necessário que o editor de arquivos locais esteja ativado.)';
+$lang['ad_c_manage_what'] = 'Escolher o tipo de conteúdo que a extensão deve apresentar-lhe';
+$lang['ad_c_howitworks'] = 'O princípio da extensão é ajudar na gestão de conteúdos para adultos (que também pode ser adaptado para conteúdo violento). Para fazer isso, o plugin gere os visitantes e / ou utilizadores
+como você desejar, de acordo com a sua definição de parâmetros da extensão. <br />
+<br />
+1 - Como administrador, deve colocar o seu conteúdo adulto numa galeria ou álbum particular  (a extensão não gerie os direitos ao nível das imagens em si, pelo menos ainda não). Depois 
+atribuir a galeria ou album a um dos dois grupos de \'18 \'e\' 16-17 \'(criados pela extensão)
+
+assim: <br /> <br />
+
+-Permitir ao grupo \'18 \' ver o conteúdo adulto, reservados para os utilizadores no mínimo com 18 anos de idade e teor normal (mas geralmente o teor normal é muitas vezes público)
+<br /> <br />
+-Permitir que o grupo "16-17" possa ver o conteúdo adulto reservado para utilizadores com idade superior a 16 e conteúdo normal (geralmente o conteúdo normal é muitas vezes público)
+<br /> <br />
+- O grupo de "nada" irá ver o conteúdo normal (mas geralmente o conteúdo normal é muitas vezes público)
+<br />
+
+<br />
+2 - Funcionamento da <br /> Extensão
+2.1 - A gestão dos utilizadores <br />
+<br />
+Se o utilizador já está registado quando da ativação da extenção, esta  mostra uma pequena caixa informando-o que ele deve escolher sua idade. Uma vez que ele clique, a página contendo a mensagem de "conteúdo adulto" de novo vai aparecer e a possibilidade de escolher sua idade (Charte_user.php).
+
+Uma vez o visitante escolhe a sua idade, é reorientado para a página de escolha de idade (Charte_user.php), logo após a sua gravação.
+
+Na prática, o plugin coloca o utilizador em três grupos, "18 \'" 16-17 "e" nada ",
+
+de acordo com a sua escolha de idade. Um utilizador pode, a qualquer momento alterar a sua idade através do menu bloco de
+
+adult_content exceto se for desativado através da administração do Piwigo (atenção link direto para charte_user.php continua a funcionar na mesma): se você quiser que um uutilizador não possa aalterar a sua idade, então esta extensão é inútil e então deve usar diretamente a gestão de direitos a nível de grupos e / ou de imagens. <br />
+<br />
+2.2 - Visitante <br /> gestão
+<br />
+Se a opção "Permitir que os visitantes não-registrados possam selecionar a sua idade?" é ativada,  estensão irá gerenciar os visitantes. * Ele exibe uma caixa, indicando que a galeria filtra
+as entradas pelo que para desativar o filtro deve introduzir a idade.
+Em seguida, com visitante, é redirecionado para a página de escolha da idade  (chartre.php).
+
+Na prática, o visitante é então gravado em um dos dois utilizadores criados pela extensão: \'
+
+18 \'Como pertencente ao grupo\' 18 \', e\' 16 \'a partir do grupo\' 16-17 \'. Estas 2 contas de utilizador não podem ser modificadas pelos utilizadores, apenas pelo administrador.
+Você entra como administrador e pode optar por alterar o tema de acordo com a idade do utilizador visto através destes duas <br contas. />
+<br />
+2.3 - Adaptações possíveis <br />
+<br />
+Você pode adaptar esta extensão para conteúdos violentos, alterando apenas os arquivos de idiomas doda extensão  (usar a extensão para editar arquivos locais proporcionar-lhe-á mais facilidade). Os limites de idade também podem ser alterados desta maneira.';
 ?>
