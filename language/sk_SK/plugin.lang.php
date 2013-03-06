43f77442b210d72581404f78432603ef7c0ab4eb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-06 16:09:58 +0000

    [adult_content] Update sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@21275 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,64 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ac_text_charte'] = 'Vstupujete na miesto s erotickým a pornografickým obsahom., ktorý je určený pre plnoletých.
+Prevádzkovateľ sa zrieka akejkoľvek zodpovednosti za použitie tohto obsahu. Každý musí pred vstupom súhlasiť so vstupom a vyhlásiť, že je plnoletý a vstupom na túto stránku neporušuje zákony svojej krajiny. Týmto preberá akúkoľvek zodpovednosť.
+</p><p>Povtrdzujem, že som bol kompletne informovaný o obsahu a zriekam sa akýchkoľvek sankcií voči prevádzkovateľovi a administrátorom tejto stránky. Prečítal som si pozorne odseky vyššie a elektronicky potvrdzujem svoj súhlas kliknutím na tlačidlo odpovedajúcemu môjmu veku.';
+$lang['ac_text_activ_16'] = '<u> ... a čestne vyhlasujem, že </u>: <br>
+        - <B> je mi medzi 16 a 17 rokmi.</B> <br>
+- bol som varovaný, že táto galéria obsahuje pornografický a erotický obsah. <br>
+- zákony mojej krajiny mi umožňujú prehliadať takýto obsah.';
+$lang['ac_text_activ_18'] = ' <u>... a čestne vyhlasujem, že </u>: <br>
+- <B>som starší ako 18 rokov a som plnoletý </B> podľa zákonov mojej krajiny. <br>
+- bol som varovaný, že táto galéria obsahuje pornografický a erotický obsah. <br>
+- zákony mojej krajiny mi umožňujú prehliadať takýto obsah.';
+$lang['ac_text'] = 'Táto galéria má obsah vhodný len pre dospelých.';
+$lang['ac_title_fermer'] = 'obsah je filtrovaný';
+$lang['ac_title_menu'] = 'Varovanie';
+$lang['ac_title_menu_statut'] = 'Informácie';
+$lang['ac_user_no'] = 'nechcete zobrazovať tento druh obsahu';
+$lang['ac_user_no_s'] = 'Obsah je filtrovaný';
+$lang['ac_user_text_16'] = 'je Vám medzi 16 a 17 rokmi';
+$lang['ac_user_text_18'] = 'Ste starší ako 18 rokov';
+$lang['ad_c_linktolang'] = 'Choďte do jazykového súboru Adult Content (je potrebné mať povolenie LocalFiles Editor)';
+$lang['ad_c_manage_both'] = 'erotický a pornografický obsah';
+$lang['ad_c_manage_eroticonly'] = 'len erotický obsah';
+$lang['ad_c_manage_pornonly'] = 'len pornografický obsah';
+$lang['ad_c_manage_what'] = 'Zvoľte, aký druh obsahu má doplnok spravovať:';
+$lang['ad_c_menublock_for_guest'] = 'Povoliť neregistrovaným používateľom voliť si svoj vek?';
+$lang['ad_c_msg_admin_1'] = 'Nastavenie doplnku bolo úspešne aktualizované!';
+$lang['ad_c_send'] = 'Uložiť zmeny';
+$lang['ac_button_activ_16'] = 'erotický obsah povolený';
+$lang['ac_button_activ_18'] = 'pornografický obsah povolený';
+$lang['ac_charte_user_def'] = 'inak kliknite tu';
+$lang['ac_charte_user_not'] = 'Prosím zvolte svoj vek';
+$lang['ac_def'] = 'Chcete zmeniť Váš stav?';
+$lang['ac_fermer'] = 'alebo koniec';
+$lang['ac_identif'] = 'alebo sa prihlásiť';
+$lang['ac_lien'] = 'zrušiť túto ochranu';
+$lang['ac_logout'] = 'inka kliknite tu';
+$lang['ac_not_def'] = 'Táto časť je dôležitá, ale už nebudete opäť dopytovaný.';
+$lang['ac_retour'] = 'Nechcem otvárať tykýto druh obsahu a preto sa vraciam na predchádzajúcu stránku';
+$lang['ac_retour_b'] = 'Nechcem tento druh obsahu';
+$lang['ac_retour_c'] = 'Vraciam sa na predchádzajúcu stránku';
+$lang['ac_statut'] = 'Vaše rozhodnutie:';
+?>
