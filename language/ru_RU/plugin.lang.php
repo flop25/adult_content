ae0f575a8e822f03fcdc3a751219e939089b77c3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-11 05:49:54 +0000

    [adult_content] Update ru_RU, thanks to : marchelly
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@21467 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ac_not_def'] = 'Это важный шаг, но в дальнейшем он не будет запрашиваться снова';
+$lang['ac_text'] = 'Эта галерея включает содержимое для взрослых';
+$lang['ad_c_manage_pornonly'] = 'только порнографическое содержимое';
+$lang['ad_c_menublock_for_guest'] = 'Разрешить незарегистрированным пользователям указывать их возраст?';
+$lang['ad_c_msg_admin_1'] = 'Настройки Плагина успешно сохранены!';
+$lang['ad_c_send'] = 'Сохранить изменения';
+$lang['Disclaimer for the guest, with the ability to choose its age to unlock the adult content'] = 'Отказ от ответственности для гостей с возможностью выбрать возраст';
+$lang['ac_title_fermer'] = 'фильтр содержимого';
+$lang['ac_title_menu'] = 'Предупреждение';
+$lang['ac_title_menu_statut'] = 'Информация';
+$lang['ac_user_no'] = 'Вы не хотите активировать содержимое этого типа';
+$lang['ac_user_no_s'] = 'Фильтр содержимого';
+$lang['ac_user_text_16'] = 'Вам 16 или 17 лет';
+$lang['ac_user_text_18'] = 'Вам более 18 лет';
+$lang['ad_c_manage_both'] = 'эротическое и порнографическое содержимое';
+$lang['ad_c_manage_eroticonly'] = 'только эротическое содержимое';
+$lang['ac_button_activ_16'] = 'Активировать эротическое содержание';
+$lang['ac_button_activ_18'] = 'Активировать порнографическое содержание содержание';
+$lang['ac_charte_user_def'] = 'иначе кликните здесь';
+$lang['ac_charte_user_not'] = 'Пожалуйста укажите Ваш возраст';
+$lang['ac_def'] = 'Хотите изменить Ваш статус?';
+$lang['ac_fermer'] = 'или закрыть';
+$lang['ac_identif'] = 'или войти';
+$lang['ac_lien'] = 'убрать эту защиту';
+$lang['ac_logout'] = 'иначе кликните здесь';
+$lang['ac_retour'] = 'Я не хочу активировать этот тип содержимого и возвращаюсь на предыдущую страницу';
+$lang['ac_retour_b'] = 'Я не хочу видеть содержимое этого типа';
+$lang['ac_retour_c'] = 'Я возвращаюсь на предыдущую страницу';
+?>
