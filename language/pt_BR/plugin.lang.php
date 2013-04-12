269e48d152631b2f98dcef4b4ec8a9d6b781d062
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-12 04:30:07 +0000

    [adult_content] Update pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@22159 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pt_BR/plugin.lang.php
+++ language/pt_BR/plugin.lang.php
@@ -21,4 +21,29 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 $lang['Disclaimer for the guest, with the ability to choose its age to unlock the adult content'] = 'Aviso legal para o convidado, com a possibilidade de escolher sua idade';
+$lang['NEW! Check the Plugin PWG Stuffs to add the disclaimer on the index/album pages'] = 'NOVO! Verifique o PWG Plugin Stuffs para adicionar o aviso nas páginas de índice/álbum';
+$lang['No options. Just check "Allow unregistered users to select their age?" on the Adult Content administration page'] = 'Não há opções. Basta verificar "Permitir que usuários não registrados selecione a sua idade?" na página de Administração Conteúdo Adulto';
+$lang['ac_button_activ_16'] = 'Ative os conteúdos eróticos';
+$lang['ac_button_activ_18'] = 'Ative os conteúdos pornográficos';
+$lang['ac_charte_user_def'] = 'ou clique aqui';
+$lang['ac_charte_user_not'] = 'Por favor selecione a sua idade!';
+$lang['ac_def'] = 'Você quer mudar o seu status?';
+$lang['ac_fermer'] = 'ou fechar';
+$lang['ac_identif'] = 'ou log-in';
+$lang['ac_lien'] = 'remover esta proteção';
+$lang['ac_logout'] = 'ou clique aqui';
+$lang['ac_not_def'] = 'Esta etapa é essencial, mas não será mais solicitada a você';
+$lang['ac_retour'] = 'Eu não desejo ativar este tipo de conteúdo, retornar a página anterior ';
+$lang['ac_retour_b'] = 'Eu não quero ver esse tipo de conteúdo';
+$lang['ac_retour_c'] = 'Voltar na página anterior';
+$lang['ac_statut'] = 'O seu estatuto:';
+$lang['ac_text'] = 'Esta galeria contém imagens para pessoas maduras e\ou adultos.';
+$lang['ac_text_activ_16'] = '<u> ... e eu garanto pela minha honra que </u>: <br>
+        - <B>Eu tenho 16 ou 17 anos.</B> <br>
+- Estou advertindo que este site propõe imagens com carácter pornográfico e erótico. <br>
+- As leis do meu país me autorizam a visitar este local.';
+$lang['ac_text_activ_18'] = '  <u>... e eu garanto pela minha honra que </u>: <br>
+- <B>Tenho mais que 18 anos de idade e sou maior </B> de acordo com a lei atual, em meu país. <br>
+- Estou advertindo que este site propõe imagens com carácter pornográfico e erótico. <br>
+- As leis do meu país me autorizam a visitar este local.';
 ?>
