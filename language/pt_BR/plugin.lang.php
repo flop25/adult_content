c62628636f134a2eceddbca80bdc3d83d8e2c987
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-08 11:52:00 +0000

    [adult_content] Update pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@22555 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pt_BR/plugin.lang.php
+++ language/pt_BR/plugin.lang.php
@@ -46,4 +46,91 @@ $lang['ac_text_activ_18'] = '  <u>... e eu garanto pela minha honra que </u>: <b
 - <B>Tenho mais que 18 anos de idade e sou maior </B> de acordo com a lei atual, em meu país. <br>
 - Estou advertindo que este site propõe imagens com carácter pornográfico e erótico. <br>
 - As leis do meu país me autorizam a visitar este local.';
+$lang['ad_c_linktolang'] = 'Ir para o arquivo de linguagem de conteúdo adulto (necessita o editor LocalFiles ativado)';
+$lang['ad_c_manage_both'] = 'conteúdo erótico e pornográfico';
+$lang['ad_c_manage_eroticonly'] = 'apenas conteúdo erótico';
+$lang['ad_c_manage_pornonly'] = 'somente conteúdo pronográfico';
+$lang['ad_c_manage_what'] = 'Selecione o tipo de conteúdo que o plugin deve gerenciar:';
+$lang['ad_c_menublock_for_guest'] = 'Permitir que usuários não registrados selecione a sua idade?';
+$lang['ad_c_msg_admin_1'] = 'Configuração do Plugin atualizado com sucesso!';
+$lang['ad_c_send'] = 'Salvar Alterações';
+$lang['ac_text_charte'] = 'Você entrou em um site os conteúdos visuais de caráter erótico e pornográfico, destinados exclusivamente para um público alertado e pode apresentar conteúdo de caráter erótico. Este site é endereçado a pessoas adultas . Este site declina qualquer responsabilidade quanto ao uso que será feito pelos conteúdos e os serviços por qualquer pessoa maior ou menor suscetível de se chocar com o conteúdo. Cada visitante está advertido e concordar em entrar neste site estritamente reservado para um público adulto e está adivertido de todas as conseqüências de sua consulta. </p>
+<p>Se eu violar um destes compromissos ou em caso de declaração falsa, eu posso estar em violação com as leis do meu país, e <B> assumirá toda a responsabilidade de meus atos </B>. Declaro ter sido completamente informado sobre os conteúdos propostos e desistir de qualquer processo judicial contra o provedor de acesso e os admistrators deste site. Eu li atentamente os parágrafos acima e assino eletrônicamente a minha concordância com o que precede, clicando no botão correspondente para a minha idade.';
+$lang['ac_title_fermer'] = 'conteúdo filtrado';
+$lang['ac_title_menu'] = 'Alerta';
+$lang['ac_title_menu_statut'] = 'informação';
+$lang['ac_user_no'] = 'você não deseja ativar este tipo de conteúdo';
+$lang['ac_user_no_s'] = 'Conteúdo filtrado';
+$lang['ac_user_text_16'] = 'Você tem 16 ou 17 anos';
+$lang['ac_user_text_18'] = 'Está mais que 18 anos de idade';
+$lang['ad_c_howitworks'] = 'O princípio do plugin é ajudar na gestão de conteúdos para adultos (que também pode ser
+
+adaptado para o conteúdo violento). Para fazer isso, o plugin gere os visitantes e/ou usuários
+
+como você deseja de acordo com a sua definição de parâmetros do plugin. <br/>
+<br/>
+1 - Como administrador, você deve colocar o seu conteúdo adulto em uma galeria ou álbum privado (o
+
+Plugin não gerencia os direitos a nível de imagens em si, pelo menos ainda não). Em seguida
+
+atribuir a galeria ou álbum para os dois grupos \'18 "e" 16-17 "(criado pelo plugin)
+
+assim: <br/><br/>
+-Permitir que ao grupo de \'18\' ver o conteúdo adulto, reservado para usuários acima de 18 anos
+
+e conteúdo normal (mas geralmente o conteúdo normal é muitas vezes público)
+<br/><br/>
+-Permitir que o grupo "16-17" para ver o conteúdo adulto reservado para usuários com idade superior a 16
+
+e conteúdo normal (geralmente o conteúdo normal é muitas vezes público)
+<br /> <br />
+- O grupo do "nada" irá ver o conteúdo normal (mas geralmente o conteúdo normal é
+
+muitas vezes público)
+<br />
+
+<br />
+2 - Funcionamento do plugin<br/>
+2.1 - A gestão dos usuários <br/>
+<br/>
+Se o usuário já estiver registrado na ativação do plugin, o plugin mostra uma pequena
+bloco apontando que ele deve escolher sua idade. Uma vez que ele clica, a página que contém a mensagem de "conteúdo adulto" vai aparecer e a possibilidade de escolher sua idade (Charte_user.php).
+
+Uma vez que um visitante escolhe a sua idade, é reorientado para a página de escolha de idade (Charte_user.php), logo após a sua gravação.
+
+Na prática, o utilizador coloca o encaixe nos três grupos, \'18\' \'16-17 "e" nada ",
+
+de acordo com a sua escolha de idade. Um usuário pode, a qualquer momento, alterar a sua idade através do menu do bloco de
+
+adult_content exceto se ele estiver desativado através da administração do Piwigo (atenção
+
+link direto para charte_user.php continua a funcionar tudo a mesma coisa): se você quiser que um
+
+usuário não possa alterar a sua idade, em seguida, este plugin é inútil e usa diretamente a gestão de
+
+direitos a nível de grupos e/ou imagens. <br/>
+<br/>
+2.2 - Gestão de Visitantes<b/>
+<br/>
+Se a opção "Permitir que os visitantes não registrados selecione a sua idade?" estiver ativado, o
+
+plugin irá gerenciar os visitantes. * Ele exibe um menu de bloco, indicando que a galeria é
+
+"Filtrada" e que, se ele deseja desativar o filtro, ele deve selecionar sua idade.
+Em seguida, é redirecionado para a página de escolha da idade para o visitante (chartre.php).
+
+Na prática, o visitante é então gravado em um dos dois usuários criados pelo plugin: \'
+
+\'18\' pertencentes ao grupo de «18» e «16» do grupo \'16-17\'. Estes 2 contas de usuário
+
+não podem ser modificados pelos usuários, somente pelo administrador.
+Você abre e optar por mudar o tema de acordo com a idade do vistante via essas duas contas de
+
+usuário. <br />
+<br />
+2.3 - Eventuais adaptações <br />
+<br />
+Você pode adaptar este plugin para o conteúdo violento, simplesmente alterando os arquivos de idiomas
+
+do plugin (usar o plugin Local Files Editor para maior facilidade). Os limites de idade também podem dessa maneira.';
 ?>
