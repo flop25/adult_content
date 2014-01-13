f090bf2f09187c016ddff6f61aefd138fbead619
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-13 18:57:40 +0000

    [adult_content] Update sk_SK, thanks to : JoeKundlak
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@26692 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sk_SK/plugin.lang.php
+++ language/sk_SK/plugin.lang.php
@@ -20,11 +20,13 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['ac_text_charte'] = 'Vstupujete na miesto s erotickým a pornografickým obsahom., ktorý je určený pre plnoletých.
+$lang['ac_text_charte'] = 'Vstupujete na stránku s erotickým a pornografickým obsahom, ktorý je určený pre plnoletých.
 Prevádzkovateľ sa zrieka akejkoľvek zodpovednosti za použitie tohto obsahu. Každý musí pred vstupom súhlasiť so vstupom a vyhlásiť, že je plnoletý a vstupom na túto stránku neporušuje zákony svojej krajiny. Týmto preberá akúkoľvek zodpovednosť.
 </p><p>Povtrdzujem, že som bol kompletne informovaný o obsahu a zriekam sa akýchkoľvek sankcií voči prevádzkovateľovi a administrátorom tejto stránky. Prečítal som si pozorne odseky vyššie a elektronicky potvrdzujem svoj súhlas kliknutím na tlačidlo odpovedajúcemu môjmu veku.';
 $lang['ac_text_activ_16'] = '<u> ... a čestne vyhlasujem, že </u>: <br>
-        - <B> je mi medzi 16 a 17 rokmi.</B> <br>
+        - <B> mám 16 alebo 17 rokov.</B> <br>
+- bol som varovaný, že táto galéria obsahuje pornografický a erotický obsah. <br>
+- zákony mojej krajiny mi umožňujú prehliadať takýto obsah.';
 - bol som varovaný, že táto galéria obsahuje pornografický a erotický obsah. <br>
 - zákony mojej krajiny mi umožňujú prehliadať takýto obsah.';
 $lang['ac_text_activ_18'] = ' <u>... a čestne vyhlasujem, že </u>: <br>
@@ -37,9 +39,9 @@ $lang['ac_title_menu'] = 'Varovanie';
 $lang['ac_title_menu_statut'] = 'Informácie';
 $lang['ac_user_no'] = 'nechcete zobrazovať tento druh obsahu';
 $lang['ac_user_no_s'] = 'Obsah je filtrovaný';
-$lang['ac_user_text_16'] = 'je Vám medzi 16 a 17 rokmi';
+$lang['ac_user_text_16'] = 'Máte 16 alebo 17 rokov';
 $lang['ac_user_text_18'] = 'Ste starší ako 18 rokov';
-$lang['ad_c_linktolang'] = 'Choďte do jazykového súboru Adult Content (je potrebné mať povolenie LocalFiles Editor)';
+$lang['ad_c_linktolang'] = 'Choďte do jazykového súboru Adult Content (je potrebné mať povolený LocalFiles Editor)';
 $lang['ad_c_manage_both'] = 'erotický a pornografický obsah';
 $lang['ad_c_manage_eroticonly'] = 'len erotický obsah';
 $lang['ad_c_manage_pornonly'] = 'len pornografický obsah';
@@ -49,17 +51,19 @@ $lang['ad_c_msg_admin_1'] = 'Nastavenie doplnku bolo úspešne aktualizované!';
 $lang['ad_c_send'] = 'Uložiť zmeny';
 $lang['ac_button_activ_16'] = 'erotický obsah povolený';
 $lang['ac_button_activ_18'] = 'pornografický obsah povolený';
-$lang['ac_charte_user_def'] = 'inak kliknite tu';
-$lang['ac_charte_user_not'] = 'Prosím zvolte svoj vek';
+$lang['ac_charte_user_def'] = 'inak kliknite sem';
+$lang['ac_charte_user_not'] = 'Prosím zvoľte svoj vek';
 $lang['ac_def'] = 'Chcete zmeniť Váš stav?';
-$lang['ac_fermer'] = 'alebo koniec';
+$lang['ac_fermer'] = 'alebo ukončiť';
 $lang['ac_identif'] = 'alebo sa prihlásiť';
 $lang['ac_lien'] = 'zrušiť túto ochranu';
-$lang['ac_logout'] = 'inka kliknite tu';
+$lang['ac_logout'] = 'inak kliknite sem';
 $lang['ac_not_def'] = 'Táto časť je dôležitá, ale už nebudete opäť dopytovaný.';
-$lang['ac_retour'] = 'Nechcem otvárať tykýto druh obsahu a preto sa vraciam na predchádzajúcu stránku';
-$lang['ac_retour_b'] = 'Nechcem tento druh obsahu';
+$lang['ac_retour'] = 'Nechcem otvárať tykýto druh obsahu a chcem sa vrátiť na predchádzajúcu stránku';
+$lang['ac_retour_b'] = 'Nechcem vidieť tento druh obsahu';
 $lang['ac_retour_c'] = 'Vraciam sa na predchádzajúcu stránku';
 $lang['ac_statut'] = 'Vaše rozhodnutie:';
 $lang['Disclaimer for the guest, with the ability to choose its age to unlock the adult content'] = 'Upozornenie pre používateľa s možnosťou voľby svojho veku';
+$lang['NEW! Check the Plugin PWG Stuffs to add the disclaimer on the index/album pages'] = 'NOVÉ! Použite doplnok PWG Stuffs pre pridanie upozornenia na stránky zoznamu/albumov';
+$lang['No options. Just check "Allow unregistered users to select their age?" on the Adult Content administration page'] = 'Žiadne predvoľby. Zaškrtnite iba "Umožniť neregistrovaným užívateľom vybrať ich vek?" na stránke administrácie obsahu pre dospelých';
 ?>
