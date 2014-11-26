f9febeed6b010100d982b1b2bbe18c5ae6956ab5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-26 06:10:22 +0000

    [adult_content] Update de_DE, thanks to bigant
    git-svn-id: http://piwigo.org/svn/extensions/adult_content@30614 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -23,16 +23,16 @@
 $lang['ac_button_activ_16'] = 'Aktivieren Sie erotische Inhalte';
 $lang['ac_button_activ_18'] = 'Aktivieren Sie pornographische Inhalte';
 $lang['ac_charte_user_def'] = 'andernfalls klicken Sie hier ';
-$lang['ac_charte_user_not'] = 'Bitte wählen Sie Ihr Alter';
+$lang['ac_charte_user_not'] = 'Bitte wählen Sie Ihr Alter!';
 $lang['ac_def'] = 'Möchten Sie ihren Status ändern?';
-$lang['ac_fermer'] = 'oder schließe ';
+$lang['ac_fermer'] = 'oder schließen';
 $lang['ac_identif'] = 'oder loggen Sie sich ein';
-$lang['ac_lien'] = 'Entferne diesen Schutz';
+$lang['ac_lien'] = 'Diesen Schutz entfernen';
 $lang['ac_logout'] = 'andernfalls klicken Sie hier ';
 $lang['ac_retour_b'] = 'Ich möchte diese Art von Inhalten nicht sehen.';
 $lang['ac_retour'] = 'Ich möchte diese Art von Inhalten nicht aktivieren und zur vorherigen Seite zurückkehren.';
-$lang['ac_statut'] = 'Ihr Status';
-$lang['ac_text'] = 'Diese Galerie enthält Bilder für reife Personen und/oder Erwachsene.';
+$lang['ac_statut'] = 'Ihr Status:';
+$lang['ac_text'] = 'Diese Galerie enthält Bilder für reife Personen und/ oder Erwachsene.';
 $lang['ac_title_fermer'] = 'Inhalt gefiltert';
 $lang['ac_title_menu_statut'] = 'Information';
 $lang['ac_title_menu'] = 'Warnung';
@@ -43,76 +43,26 @@ $lang['ac_user_text_18'] = 'Sie sind älter als 18 Jahre';
 $lang['ad_c_menublock_for_guest'] = 'Erlaube unregistrierten Benutzern das Alter auszuwählen?';
 $lang['ad_c_send'] = 'Änderungen speichern';
 $lang['ac_not_def'] = 'Dieser Vorgang ist wichtig, wird aber nicht noch einmal erfragt';
-$lang['ac_retour_c'] = 'Ich komme zurück auf die vorherige Seite';
-$lang['ac_text_charte'] = 'Sie betreten eine Website mit visuellen erotischen und/oder pornografischen Inhalten, die für die Öffentlichkeit bestimmt sind und vor denen Sie gewarnt wurden. 
+$lang['ac_retour_c'] = 'Zurück auf die vorherige Seite';
+$lang['ac_text_charte'] = 'Sie betreten eine Website mit visuellen erotischen und/ oder pornografischen Inhalten, die für die Öffentlichkeit bestimmt sind und vor denen Sie gewarnt wurden. 
 Diese Webseite spricht nur bereitwillige Personen an. 
 Die vorliegende Webseite lehnt jede Verantwortung für die durch den Inhalt und deren Dienste schockierten Personen ab. 
 Jede Person, die trotz eingehender Warnung, ihre Zustimmung zum betreten dieser Seite gibt, befreit diese Seite von allen Konsequenzen daraus.</ p>
 <p> Wenn ich eine dieser Zusagen nicht einhalte oder eine falsche Erklärung abgebe, kann ich eventuell die Gesetze meines Landes verletzen, und <B> übernehme damit die volle Verantwortung für mein Handeln </ B>. Ich erkläre hiermit, vollständig über die angebotenen Inhalte informiert worden zu sein und verzichte auf irgendwelche rechtlichen Schritte gegen den Provider und den Administrator dieser Webseite. Ich lese aufmerksam vorstehende Absätze und gebe elektronisch mein Einverständnis, indem ich mein entsprechendes Alter anklicke.';
-$lang['ad_c_linktolang'] = 'Gehen Sie auf die Sprach-Datei von nicht jugendfreien Inhalten (Benötigen LocalFiles Editor aktiviert)';
+$lang['ad_c_linktolang'] = 'Zur Sprach-Datei der nicht jugendfreien Inhalte gehen (LocalFiles Editor aktiviert)';
 $lang['ad_c_manage_both'] = 'erotische und pornografische Inhalte';
 $lang['ad_c_manage_eroticonly'] = 'nur erotische Inhalte';
 $lang['ad_c_manage_pornonly'] = 'nur pornografische Inhalte';
 $lang['ad_c_manage_what'] = 'Wählen Sie die Art der Inhalte, die das Plugin verwalten soll:';
 $lang['ad_c_msg_admin_1'] = 'Plugin-Konfiguration erfolgreich aktualisiert!';
-$lang['ac_text_activ_16'] = '<u> ... und ich versichere hiermit </ u>: <br>
-- <B> Dass ich 16 oder 17 Jahre alt bin.</B><br>
+$lang['ac_text_activ_16'] = '<u> ... und ich versichere hiermit, </ u>: <br>
+- <B> dass ich 16 oder 17 Jahre alt bin.</B><br>
 - Ich bin über die Inhalte dieser Webseite mit erotischen und pornografischen Bildern gewarnt worden.<br>
 - Die Gesetze meines Landes erlauben mir den Zutritt zu dieser Seite.';
-$lang['ac_text_activ_18'] = '<u> ... und ich versichere hiermit </ u>: <br>
-- <B>Dass ich nach den Gesetzen meines Landes über 18 Jahre alt bin und ein Erwachsener. </ B> <br>
+$lang['ac_text_activ_18'] = '<u> ... und ich versichere hiermit, </ u>: <br>
+- <B>dass ich nach den Gesetzen meines Landes über 18 Jahre alt bin und ein Erwachsener. </ B> <br>
 - Ich bin über die Inhalte dieser Webseite mit erotischen und pornografischen Bildern gewarnt worden. <br>
 - Die Gesetze meines Landes erlauben mir den Zutritt zu dieser Seite.';
-$lang['ad_c_howitworks'] = 'Das Prinzip des Plugins ist, bei der Verwaltung von jugendfreien 
-Inhalten zu helfen (es kann auch für gewalttätige Inhalte angepasst 
-werden). Dazu verwaltet das Plugin die Besucher und/oder Nutzer nach 
-ihren Wünschen mit entsprechenden Definitionen und Parameter des 
-Plugins. <br />
-<br />
-1 - Als Administrator müssen Sie Ihre nicht jugendfreien Inhalte in eine 
-private Galerie oder ein Album packen (das Plugin verwaltet nicht die 
-Rechte auf der Ebene von einzelnen Bilder selbst, zumindest noch nicht). 
-Sie weisen die Galerie oder das Album zu den beiden Gruppen 18 und/oder 
-16-17 zu (erstellt durch das Plugin).
-Das sieht wie folgt aus: <br /> <br />
-
--Erlaube der Gruppe \'18\', normale Inhalte (die in der Regel öffentlich sind) und
-Inhalte für Erwachsene zu sehen, die für Nutzer über 18 Jahren reserviert sind
-<br /> <br />
-
--Erlaube der Gruppe "16-17", normale Inhalte (die in der Regel öffentlich sind)
- und Inhalte für Jugendlich zu sehen, die für Nutzer über 16 Jahren bestimmt sind
-<br /> <br />
-
--Die Gruppe "Nichts" sieht nur die normalen Inhalte (die in der Regel sowieso öffentlich sind)
-<br />
-
-<br />
-2 - Arbeitsweise des Plugins <br />
-2.1 - Die Benutzer Verwaltung <br />
-<br />
-Falls ein Benutzer bereits bei der Aktivierung des Plugins registriert ist, zeigt das Plugin nun einen kleinen
-blockierenden Hinweis, dass er sein Alter angeben muss. Sobald er hier klickt, wird die Seite mit der Meldung von "Nicht jugendfreien 
-Inhalten" erscheinen und er muss das Alter auswählen. (Charte_user.php).
-
-Sobald ein Besucher sein Alter gewählt hat, wird sich die Seite entsprechend seinem Alter, unmittelbar nach seiner Auswahl
-(Charte_user.php) neu ausrichten.
-
-In der Praxis legt das Plugin den Anwender nach seiner Wahl des Alters, in einer der 3 Gruppen \'18\' \'16-17\' oder \'nichts\' an,
-
-Ein Benutzer kann jederzeit sein Alter über das Block Menü ändern, (adult_content) es sei denn, es wurde über die Administration von Piwigo deaktiviert.
-(Achtung der direkter Link zu charte_user.php hat weiterhin die gleiche Funktion): 
-Wenn Sie möchten, dass ein Benutzer nicht sein Alter ändern kann, dann ist dieses Plugin nutzlos. Dann verwenden Sie direkt die Rechte-Verwaltung
-auf der Ebene von Gruppen und/oder Bildern. <br />
-<br />
-2.2 - Besucher Verwaltung <br />
-<br />
-Wenn die Option "Erlaube nicht registrierten Besuchern ihr Alter zu wählen?" aktiviert ist, dann übernimmt das Plugin die Besucher-Verwaltung.
-* Es wird ein Block Menü angezeigt, damit ist die Galerie "Gefiltert". Um den Filter zu deaktivieren, muss der Besucher sein Alter auswählen. 
-Er wird dann auf die Seite zur Auswahl des Alters für Besucher (chartre.php) umgeleitet.
-
-In der Praxis wird der Besucher dann unter einem der durch das Plugin erstellten 2 Benutzer aufgezeichnet:
-\'18\' Zugehörigkeit zur Gruppe der über 18 jährigen und \'16\' zur Gruppe der 16-17 jährigen. 
 Diese 2 Gruppen Konten können nicht vom Benutzer geändert werden, sondern nur durch den Administrator.
 Sie öffnen und wählen das Aussehen, je nach Alter der Besucher über diese beiden Gruppen Konten. <br />
 <br />
@@ -121,6 +71,5 @@ Sie öffnen und wählen das Aussehen, je nach Alter der Besucher über diese bei
 Sie können dieses Plugin für die nicht jugendfreien Inhalte anpassen, indem Sie einfach die Sprache des Plugins wechseln.
 (verwenden Sie einfach das Plugin "Local Files Editor") Die Altersgrenzen können auch auf diese Weise verändert werden.';
 $lang['No options. Just check "Allow unregistered users to select their age?" on the Adult Content administration page'] = 'Keine Optionen. Aktiviere "Erlaube nicht registrierten Besuchern das Alter auszuwählen?" auf der Aduld Content Administrations Seite.';
-$lang['Disclaimer for the guest, with the ability to choose its age to unlock the adult content'] = 'Hinweis für Gäste mit der Möglichkeit das Alter auszuwählen';
-$lang['NEW! Check the Plugin PWG Stuffs to add the disclaimer on the index/album pages'] = 'NEU! Prüfe das Plugin PWG Stuffs um eine Erklärung für die Startseite und die  Albumseiten zu erstellen.';
-?>
+$lang['Disclaimer for the guest, with the ability to choose its age to unlock the adult content'] = 'Hinweis für Gäste, mit der Möglichkeit das eigene Alter anzugeben';
+$lang['NEW! Check the Plugin PWG Stuffs to add the disclaimer on the index/album pages'] = 'NEU! Plugin PWG Stuffs ausprobieren, um einen Disclaimer für die Startseite und die  Albumseiten zu erstellen.';
